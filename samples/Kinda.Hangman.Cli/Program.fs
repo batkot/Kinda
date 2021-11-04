db88c79460d363b74f802cec1379121bf9843c35
Tomasz Batko <batkot@protonmail.com>
2021-11-04 23:24:13 +0100

    Enumerate allowed modes in sample

--- samples/Kinda.Hangman.Cli/Program.fs
+++ samples/Kinda.Hangman.Cli/Program.fs
@@ -5,6 +5,7 @@ open Kinda.Hangman.Cli.Tagless
 open Kinda.Hangman.Cli.Free
 open Kinda.Hangman.Cli.Transformers
 open CommandLine
+open CommandLine.Text
 
 type Mode
     = FreeMonad = 0
@@ -12,7 +13,7 @@ type Mode
     | Transformers = 2
 
 type Options = 
-    { [<Option(Default = Mode.FreeMonad, HelpText = "Program version to run (FreeMonad|Tagless)")>] Mode : Mode
+    { [<Option(Default = Mode.FreeMonad, HelpText = "Program version to run")>] Mode : Mode
       [<Option(Required=true)>] Puzzle: string
       [<Option(Default=10)>] Chances: int
     }
@@ -25,12 +26,24 @@ let run options =
     | Mode.Transformers -> runTransformers game |> ignore
     | _ -> failwith ":("
 
+let displayHelp<'a> (error: NotParsed<'a>) = 
+    let x =
+        HelpText.AutoBuild
+            ( error
+            , fun opt -> 
+                opt.AddEnumValuesToHelpText <- true
+                HelpText.DefaultParsingErrorsHandler(error, opt)
+            , id)
+
+    x.ToString()
+
 [<EntryPoint>]
 let main argv =
-    let parseResult = CommandLine.Parser.Default.ParseArguments<Options>(argv)
+    let parser = new Parser(fun x -> x.HelpWriter <- null)
+    let parseResult = parser.ParseArguments<Options>(argv)
 
     match parseResult with
     | :? Parsed<Options> as parsed -> run parsed.Value
-    | :? NotParsed<Options> as notParsed -> printfn "Failed"
+    | :? NotParsed<Options> as notParsed -> printfn "%A" <| displayHelp notParsed
 
     0
